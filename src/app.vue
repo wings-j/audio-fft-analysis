a59eac8de2abeb2831f6ee0e6e4778879a49972d
WingsJ <mail@wings-j.cn>
2022-06-16 18:51:02 +0800

    update

--- src/app.vue
+++ src/app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div></div>
+  <div class="d-flex flex-column h-100vh">
+    <div class="flex-grow-1 bg-black"></div>
+    <div></div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
