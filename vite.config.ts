0af90106427a6db7925488b0a9b157b388962106
WingsJ <mail@wings-j.cn>
2022-06-16 18:32:56 +0800

    initiate

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import Vue from '@vitejs/plugin-vue'
+import Path from 'path'
+
+export default defineConfig({
+  base: './',
+  resolve: {
+    alias: {
+      '@': Path.resolve(__dirname, 'src')
+    }
+  },
+  plugins: [Vue()],
+  build: {
+    target: 'esnext'
+  }
+})
