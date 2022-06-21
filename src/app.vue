829b4afccb58641cb3858844ceba0e61531a8232
WingsJ <mail@wings-j.cn>
2022-06-21 21:25:10 +0800

    update

--- src/app.vue
+++ src/app.vue
@@ -27,10 +27,29 @@
         </ElSelect>
       </ElCard>
       <ElCard class="flex-basis-0 flex-grow-1 m-l-20">
-        <div class="cursor-pointer opacity-5 pointer-events-none" :class="{ active: audio }" @click="handle_play_click">
+        <div class="cursor-pointer opacity-5 pointer-events-none" :class="{ active: ready }" @click="handle_play_click">
           <img v-if="play" style="width: 20px; height: 20px" src="./images/pause.svg" />
           <img v-else style="width: 20px; height: 20px" src="./images/play.svg" />
         </div>
+        <img
+          class="m-l-15 cursor-pointer opacity-5 pointer-events-none"
+          :class="{ active: ready }"
+          style="width: 20px; height: 20px"
+          src="@/images/previous.svg"
+          @click="handle_rollback_click"
+        />
+        <svg
+          class="m-l-15 cursor-pointer opacity-5 pointer-events-none"
+          :class="{ active: ready }"
+          style="height: 20px"
+          :style="{ width: `${slideLength + 10}px` }"
+          ref="$slide"
+          :viewBox="`0 -10 ${slideLength + 10} 20`"
+          @click="handle_slide_click"
+        >
+          <line x1="0" y1="0" :x2="slideLength" y2="0" stroke="black"></line>
+          <rect :x="progress * slideLength" y="-6" width="4" height="12"></rect>
+        </svg>
       </ElCard>
       <ElCard class="flex-basis-0 flex-grow-1 m-l-20"> </ElCard>
     </div>
@@ -39,26 +58,44 @@
 
 <script setup lang="ts">
   import { ref, onMounted, watch } from 'vue'
-  import { useElementSize } from '@vueuse/core'
-  import { ElCard, ElFormItem, ElSelect, ElOption } from 'element-plus'
+  import { useElementSize, useMouseInElement } from '@vueuse/core'
+  import { ElCard, ElSelect, ElOption } from 'element-plus'
 
-  const gap = 2
+  const columnGap = 2
+  const slideLength = 300
 
   const $canvas = ref<HTMLCanvasElement | undefined>()
   const { width = ref(0), height = ref(0) } = useElementSize($canvas)
   const $wave = ref<HTMLCanvasElement | undefined>()
+  const $slide = ref<SVGSVGElement | undefined>()
+  const { elementX: slideX } = useMouseInElement($slide)
   const fftSize = ref(1024)
+  const ready = ref(false)
   const play = ref(false)
-  const audio = ref<HTMLAudioElement | undefined>()
-  const data = ref<number[]>([])
+  const progress = ref(0)
   let $waveContext: CanvasRenderingContext2D | undefined
+  let audio = new Audio()
+  let context = new AudioContext()
   let analyzer: AnalyserNode | undefined
   let buffer = new Uint8Array(fftSize.value)
+  let data: number[] = []
+
+  audio.loop = true
+  audio.addEventListener('canplay', () => {
+    ready.value = true
+    context.resume()
+  })
+  audio.addEventListener('timeupdate', () => {
+    progress.value = audio.currentTime / audio.duration
+  })
+  analyzer = context.createAnalyser()
+  analyzer.fftSize = fftSize.value
+  context.createMediaElementSource(audio).connect(analyzer)
+  analyzer.connect(context.destination)
 
   watch(fftSize, value => {
     buffer = new Uint8Array(value)
   })
-
   onMounted(() => {
     $waveContext = $wave.value?.getContext('2d')!
   })
@@ -77,15 +114,7 @@
       }
     })
 
-    let context = new AudioContext()
-    audio.value = new Audio(base64)
-    let source = context.createMediaElementSource(audio.value)
-    analyzer = context.createAnalyser()
-    analyzer.fftSize = fftSize.value
-    source.connect(analyzer)
-    analyzer.connect(context.destination)
-
-    context.resume()
+    audio.src = base64
   }
   /**
    * 处理播放点击
@@ -94,11 +123,24 @@
     play.value = !play.value
 
     if (play.value) {
-      audio.value?.play()
+      audio.play()
       animate()
     } else {
-      audio.value?.pause()
+      audio.pause()
     }
+  }
+  /**
+   * 处理回退点击
+   */
+  function handle_rollback_click() {
+    audio.currentTime = 0
+  }
+  /**
+   * 处理滑块点击
+   */
+  function handle_slide_click() {
+    let rate = Math.min(Math.max(slideX.value / slideLength, 0), 1)
+    audio.currentTime = audio.duration * rate
   }
 
   /**
@@ -109,7 +151,7 @@
       if (play.value) {
         if (analyzer) {
           analyzer.getByteFrequencyData(buffer)
-          data.value = Array.from(buffer).map(a => a / 255)
+          data = Array.from(buffer).map(a => a / 255)
         }
 
         drawWave()
@@ -125,14 +167,14 @@
    */
   function drawWave() {
     if ($waveContext) {
-      let length = data.value.length
+      let length = data.length
       let dw = Math.max(Math.floor(width.value / length), 1)
 
       $waveContext.clearRect(0, 0, width.value, height.value)
       $waveContext.fillStyle = '#409EFF'
 
-      for (let i = 0; i < data.value.length; i++) {
-        $waveContext.fillRect((dw + gap) * i, 0, dw, height.value * data.value[i])
+      for (let i = 0; i < data.length; i++) {
+        $waveContext.fillRect((dw + columnGap) * i, 0, dw, height.value * data[i])
       }
     }
   }
